78e998f6d5133cd5f8c4c3fb050313998ef22cda
azhyrkevich <anastasiya-zhyrkevich@yandex.ru>
2018-08-08 17:14:47 +0300

    3

--- main5.py
+++ main5.py
@@ -7,10 +7,22 @@ class MyTest(unittest.TestCase):
         board = generate_board(n, m)
 
     def test2(self):
-        pass
+        n, m = 10, 10
+        board = generate_board(n, m)
+        self.assertLessEqual(get_active_cells(board, 0, 0), 8)
+
 
 def generate_board(n ,m):
     return [[False] * m for _ in xrange(n)]
 
 def get_board_cell(board, i, j):
+    if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]):
+        return False
     return board[i][j]
+
+def get_active_cells(board, i, j):
+    alive = 0
+    for ii in xrange(-1, 2):
+        for jj in xrange(-1, 2):
+            alive += int(get_board_cell(board, i + ii, j + jj))
+    return alive

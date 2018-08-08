014cddb028204d2de9457aa7ac0b6b61f1396485
azhyrkevich <anastasiya-zhyrkevich@yandex.ru>
2018-08-08 17:11:00 +0300

    2

--- main5.py
+++ main5.py
@@ -2,6 +2,15 @@ import unittest
 
 class MyTest(unittest.TestCase):
 
-    def test1():
-        n, m = ran
+    def test1(self):
+        n, m = 10, 10
         board = generate_board(n, m)
+
+    def test2(self):
+        pass
+
+def generate_board(n ,m):
+    return [[False] * m for _ in xrange(n)]
+
+def get_board_cell(board, i, j):
+    return board[i][j]
